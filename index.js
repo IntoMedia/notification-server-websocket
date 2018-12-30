07d29aa89f668a1cfa90e0197a683d7509c6ba2f
vmarci21 <vmarci21@msn.com>
2018-12-30 18:49:14 +0100

    port settings

--- index.js
+++ index.js
@@ -111,6 +111,6 @@ io.on('connection', function(socket){
 	
 });
 
-http.listen(8080, function(){
-	console.log('listening on *:8080');
+http.listen(process.env.PORT, function(){
+	console.log('listening on *:'+process.env.PORT);
 });
