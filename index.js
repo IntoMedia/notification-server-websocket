a5e8bc6be7c74856abe174d8244298dcce2b3505
Veszter Márton <vmarci21@msn.com>
2018-12-31 15:57:57 +0100

    classes

--- index.js
+++ index.js
@@ -3,59 +3,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var OneSignal = require('onesignal-node');
+var Users = require('./lib/Users');
+var Notifications = require('./lib/Notifications');
 
 require('dotenv').config();
+
+
+const users = new Users();
 
 
 var activeUsers = {};
 
 
-function createPush(type,title,msg,url,to){
-	
-	var filter = [];
-	
-	if(to.method=='personal'){
-		filter = [
-			{"field": "tag", "key": "user", "relation": "=", "value": to.user}
-		];
-	}else if(to.method=='group'){
-		filter = [
-			{"field": "tag", "key": "group_"+to.type+"_"+to.name, "relation": "=", "value": true}
-		];
-		for (var i=0;i<to.notSend.length;i++){
-			filter.push({"field": "tag", "key": "user", "relation": "!=", "value": to.notSend[i]});
-		}
-	}
-	
-	
-	var PushNotification = new OneSignal.Notification({ 
-		contents: {    
-			en: msg
-		},
-		headings: {
-			en: title
-		},
-		data:{
-			"type":type
-		},
-		web_url: url,
-		filters: filter
-	});
-	
-	oneSignalClient.sendNotification(PushNotification, function (err, httpResponse,data) {    
-		if(err){ 
-			console.log('Something went wrong...');    
-		}else{
-			console.log(data, httpResponse.statusCode);    
-		}
-	});
-}
+const notifications = new Notifications(io,OneSignal);
 
 
-var oneSignalClient = new OneSignal.Client({
-	userAuthKey: process.env.ONE_SIGNAL_USERAUTHKEY,    
-	app: { appAuthKey: process.env.ONE_SIGNAL_APPAUTHKEY, appId: process.env.ONE_SIGNAL_APPID }    
-});
+
 
 app.get('/', function(req, res){
 	res.send('<h1>Hello world</h1>');
@@ -72,39 +35,34 @@ app.get('/send/:method/:type', function(req, res){
 	}else{
 		
 		if(req.params.method=='personal'){
-			var ok = false;
 			var user = req.query.user;
-			for (var u in activeUsers){
-				if(activeUsers[u].user==user){
-					if(activeUsers[u].active){
-						ok = true;
-					}
-					
-					io.sockets.connected[u].emit(req.params.type, {msg:req.query.msg,title:req.query.title,url:req.query.url});
-				}
-			}
-			if(!ok){
-				createPush(req.params.type,req.query.title,req.query.msg,req.query.url,{method:'personal',user:user});
-			}
+			var status = users.getUserStatus(user);
+			
+			var message = {
+				type:req.params.type,
+				msg:req.query.msg,
+				title:req.query.title,
+				url:req.query.url
+			};
+			notifications.personal(user,status,message);
+			
 			
 			resdata.success = true;
 		}else if(req.params.method=='group'){
-			var notSend = [];
-			
 			var group_type = req.query.type;
 			var group_name = req.query.name;
 			
-			for (var u in activeUsers){
-				if(activeUsers[u].groups[group_type][group_name] != undefined){
-					if(activeUsers[u].active){
-						notSend.push(activeUsers[u].user);
-					}
-					
-					io.sockets.connected[u].emit(req.params.type, {msg:req.query.msg,title:req.query.title,url:req.query.url});
-				}
-			}
+			var data = users.getUsersByGroup(group_type,group_name);
 			
-			createPush(req.params.type,req.query.title,req.query.msg,req.query.url,{method:'group',type:group_type,name:group_name,notSend:notSend});
+			
+			var message = {
+				type:req.params.type,
+				msg:req.query.msg,
+				title:req.query.title,
+				url:req.query.url
+			};
+			
+			notifications.group(group_type,group_name,data,message);
 			
 			resdata.success = true;
 		}else{
@@ -120,22 +78,14 @@ app.get('/send/:method/:type', function(req, res){
 
 
 io.on('connection', function(socket){
-	console.log('a user connected');
-	console.log(socket.request._query['user']);
-	console.log(socket.id);
+	var data = users.addSession(socket.id,socket.request._query['user']);
 	
-	var data = {
-		user:socket.request._query['user'],
-		active:true
-	};
-	
-	activeUsers[socket.id] = data;
+	if(data.requireInformations){
+		socket.emit('requireInformations',true);
+	}
 	
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
-		delete activeUsers[socket.id];
-		
-		console.log(activeUsers);
+		users.removeSession(socket.id);
 	});
 	
 });
