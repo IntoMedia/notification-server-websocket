d8d81930f489b3fdea330c87acd0ffa5d95d0402
Veszter Márton <vmarci21@msn.com>
2019-01-01 14:58:47 +0100

    groups working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env nodejs
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var OneSignal = require('onesignal-node');
@@ -8,20 +9,41 @@ var Notifications = require('./lib/Notifications');
 
 require('dotenv').config();
 
-
-const users = new Users();
-
-
-var activeUsers = {};
+app.use(express.json());
 
 
+const users = new Users(io);
 const notifications = new Notifications(io,OneSignal);
 
 
 
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello world</h1>');
+	res.send('Working.. https://github.com/IntoMedia/notification-server-websocket');
+});
+
+app.get('/addgroups', function(req, res){
+	var resdata = {};
+	
+	resdata.error = true;
+	resdata.message = 'WRONG HTTP METHOD!';
+	
+	res.send(JSON.stringify(resdata));
+});
+
+app.post('/addgroups', function(req, res){
+	var resdata = {success:false};
+	
+	if(process.env.ACCESS_CODE != req.body.code){
+		resdata.error = true;
+		resdata.message = 'ACCESS FORBIDDEN';
+	}else{
+		resdata.success = true; 
+		
+		users.addUserGroups(req.body.user,req.body.groups);
+	}
+	
+	res.send(JSON.stringify(resdata));
 });
 
 
@@ -34,42 +56,61 @@ app.get('/send/:method/:type', function(req, res){
 		resdata.message = 'ACCESS FORBIDDEN';
 	}else{
 		
-		if(req.params.method=='personal'){
-			var user = req.query.user;
-			var status = users.getUserStatus(user);
-			
+		var ok = false;
+		
+		if(req.params.type=='notification'){
 			var message = {
 				type:req.params.type,
 				msg:req.query.msg,
 				title:req.query.title,
-				url:req.query.url
+				url:req.query.url,
+				image:req.query.image
 			};
-			notifications.personal(user,status,message);
 			
+			ok = true;
+		}else if(req.params.type=='event'){
 			
-			resdata.success = true;
-		}else if(req.params.method=='group'){
-			var group_type = req.query.type;
-			var group_name = req.query.name;
-			
-			var data = users.getUsersByGroup(group_type,group_name);
-			
+			delete req.query.code;
+			var event = req.query.event;
+			delete req.query.event;
 			
 			var message = {
 				type:req.params.type,
-				msg:req.query.msg,
-				title:req.query.title,
-				url:req.query.url
+				event:event,
+				data:req.query
 			};
 			
-			notifications.group(group_type,group_name,data,message);
-			
-			resdata.success = true;
-		}else{
-			resdata.error = true;
-			resdata.message = 'WRONG METHOD';
+			ok = true;
 		}
 		
+		if(ok){
+		
+			if(req.params.method=='personal'){
+				var user = req.query.user;
+				var status = users.getUserStatus(user);
+
+
+				notifications.personal(user,status,message);
+
+
+				resdata.success = true;
+			}else if(req.params.method=='group'){
+				var group_type = req.query.type;
+				var group_name = req.query.name;
+
+				var data = users.getUsersByGroup(group_type,group_name);
+
+				notifications.group(group_type,group_name,data,message);
+
+				resdata.success = true;
+			}else{
+				resdata.error = true;
+				resdata.message = 'WRONG METHOD';
+			}
+		}else{
+			resdata.error = true;
+			resdata.message = 'WRONG TYPE';
+		}
 		//io.emit(req.params.type, req.query.msg);
 	}
 	
